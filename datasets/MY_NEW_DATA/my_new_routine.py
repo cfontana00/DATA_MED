9d686505855361574e9d2fc9eb2ff4dd109c0ff5
cfontana00 <cfontana@ogs.it>
2024-03-21 11:31:14 +0100

    modif example

--- datasets/MY_NEW_DATA/my_new_routine.py
+++ datasets/MY_NEW_DATA/my_new_routine.py
@@ -1,4 +1,5 @@
 from fun_gen import load_config
+from fun_io import *
 import numpy as np
 import sys
 
@@ -16,10 +17,36 @@ except Exception as e :
   exit()
 
 print("The initial julian day is",jdini)
+print("The final julian day is",jdend)
 
 # Load data
 data=np.loadtxt("my_new_data.dat")
 print('My data is',data)
+print('-----------------------------')
+
+
+# Load coordinates
+lon,lat,levels = load_coords()
+
+
+# Define model parameters
+print('Load data')
+varname = 'thetao'
+level = 0
+
+
+for jd in range(jdini,jdend):
+
+  # Get filename
+  fname,dtag = get_filename(jd,'TEMP')
+
+  # Get 2D variable
+  var2d = get_var_2D(fname,varname,level)
+
+  # Get 3D variable
+  var3d = get_var_3D(fname,varname)
+
+  break
 
 # To be continued ...
 
